356f0f2275469f3a74fb1a3aee9bb2f38a80c223
Aravind <80593269+AravindCSEiith@users.noreply.github.com>
2021-04-17 10:20:28 +0530

    Delete ASSIGNMENT_3_AI1103.py

--- ASSIGNMENT_3_AI1103.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x=np.linspace(-8,10,10000)#displaying the graph between -8 and 10
-p=1/6
-def f(x): return (p-p**2)*x**2-2*p*x+6*p
-plt.plot(x, f(x))
-plt.title('Variance $V(X)$ for $p=1/6$')
-plt.xlabel('k')
-plt.ylabel('$V(X)$ or $f(k)$')
